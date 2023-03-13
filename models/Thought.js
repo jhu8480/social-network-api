2d40d4958c17f5f3fe76651657991917b2310281
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-12 20:20:33 -0400

    added Thought model

---
+++ models/Thought.js
@@ -0,0 +1,50 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+      //TODO: set a getter here
+    }
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //TODO: set a getter here
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
+
